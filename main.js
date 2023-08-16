2fe34a84641d8c95c43c429caf80f956989b3f06
White Debon <38989015+WDebon@users.noreply.github.com>
2023-08-16 01:12:51 -0300

    arrumei!

--- main.js
+++ main.js
@@ -8,23 +8,32 @@ const desc_Input = document.getElementById("desc");
 
 const button_Input = document.querySelector("button")
 
-function telefone_Func(func) {
+// function telefone_Func(func) {
 
-    const telArray = func.toString().split("") //vai separar o input por cada letra
-
-    for (let i = 0; i < telArray.length; i++) 
-    {
-        // isNan vai dar true se for um número e false se não for
+//     const telArray = func.split("") //vai separar o input por cada letra
+//     console.log(func);
+//     for (let i = 0; i < telArray.length; i++) 
+//     {
+//         // isNan vai dar true se for um número e false se não for
         
-        isNaN(telArray[i])
-    }
-}
+//         isNaN(telArray[i])
+//         // console.log(isNaN(telArray[i]));
+//     }
+// }
 
 telefone_Input.addEventListener("keyup", function(e){
-        if(!telefone_Func(telefone_Input)){
-            button_Input.disabled = "true";
-        }else{
-            button_Input.disabled = "false";}
+    const telefone_Value = telefone_Input.value
+            // console.log(telefone_Value);
+            // console.log(isNaN(telefone_Value))
+            // button_Input.disabled = !telefone_Func(telefone_Input);
+
+            if(isNaN(telefone_Value)){
+                button_Input.style.backgroundColor = "grey";
+                button_Input.disabled = "true";
+            }else{
+                button_Input.style.backgroundColor = "";
+                button_Input.disabled = "false";
+            }
         
 });
 
