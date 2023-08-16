e76636de9e715bcbfea43a13daa13bda2938ed14
White Debon <38989015+WDebon@users.noreply.github.com>
2023-08-16 00:33:43 -0300

    Arrumando o eventlistener

--- main.js
+++ main.js
@@ -6,14 +6,27 @@ const cor_Input = document.getElementById("tag");
 
 const desc_Input = document.getElementById("desc");
 
-function telefone_Func(e) {
-    const telArray = e.split("")
+const button_Input = document.querySelector("button")
+
+function telefone_Func(func) {
+
+    const telArray = func.toString().split("") //vai separar o input por cada letra
 
     for (let i = 0; i < telArray.length; i++) 
     {
+        // isNan vai dar true se for um número e false se não for
+        
         isNaN(telArray[i])
     }
 }
+
+telefone_Input.addEventListener("keyup", function(e){
+        if(!telefone_Func(telefone_Input)){
+            button_Input.disabled = "true";
+        }else{
+            button_Input.disabled = "false";}
+        
+});
 
 
 
